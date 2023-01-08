9758f23926662425362a772a9cd59ebf5dc0e8b2
swanlz <116781172+swanlz@users.noreply.github.com>
2023-01-08 20:29:25 -0300

    c lang

---
+++ scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+	
+	int age = 0;
+	float weight = 0;
+	
+	
+	printf ("Insert your age: \n");
+	scanf ("%d", &age);
+	
+	printf ("Insert your weight: \n");
+	scanf("%f", &weight);
+	
+	printf ("Age inserted is: %d. \n", age);
+	printf ("Weight inserted is: %f. \n", weight);
+	
+}
