d1374e211dd9e15cda004ae0add1aebcad3906c2
swanlz <116781172+swanlz@users.noreply.github.com>
2023-01-13 06:19:24 -0300

    c lang

---
+++ increment and decrement.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	
+	int value = 0;
+	printf("Insert a value: \n");
+	scanf("%d", &value);
+	
+	printf("Value before increment: %d. \n", value);
+	
+	value++;
+	printf("Value after increment: %d. \n", value);
+	
+	value--;
+	printf("Value after decrement: %d. \n", value);
+	
+	value += 5;
+	printf("Value after 5 units increment: %d. \n", value);
+	
+	value -= 5;
+	printf("Value after 5 units decrement: %d. \n", value);
+	
+	value *= 5;
+	printf("Value after multiplied by 5: %d. \n", value);
+	
+	
+}
---
+++ math operations.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int A, B, plus, minus, times, divided;
+
+int main(){
+	
+	printf("Insert a number: \n");
+	scanf("%d", &A);
+	
+	printf("Insert another number: \n");
+	scanf("%d", &B);
+	
+	plus = A + B;
+	minus = A - B;
+	times = A * B;
+	divided = A / B;
+	
+	printf("Results: \n");
+	printf("Addition: %d. \n", plus);
+	printf("Subtraction: %d, \n", minus);
+	printf("Multiplication: %d. \n", times);
+	printf("Division: %d. \n", divided);
+	
+	
+}
