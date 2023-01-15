0ec57f04e06c36886dac11d7af05bc159de8db7f
swanlz <116781172+swanlz@users.noreply.github.com>
2023-01-15 19:26:10 -0300

    c lang

---
+++ sequencial numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define text "Sequencial numbers sum"
+
+int n1, n2, n3, n4, n5, total;
+
+int main() {
+	printf("%s\n", text);
+	printf("Insert 5 numbers, in sequence: \n");
+	scanf("%d", &n1);	scanf("%d", &n2);
+	scanf("%d", &n3);	scanf("%d", &n4);	scanf("%d", &n5);
+	
+	total = (n1 + n2 + n3 + n4 + n5);
+	
+	printf("Total sum is: %d.\n", total);
+	
+	return 0;
+	
+}
+
